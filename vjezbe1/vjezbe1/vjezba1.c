00e493b56bc806c8a741bc58e0cf8e1e1080a7e0
JVucko0 <jvucko02@fesb.hr>
2023-10-09 20:06:48 +0200

    prva vj

--- vjezbe1/vjezbe1/vjezba1.c
+++ vjezbe1/vjezbe1/vjezba1.c
@@ -1,8 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS  
 #include <stdio.h>
+#include <stdlib.h>
+
+/* Napisati program koji prvo pročita koliko redaka ima datoteka, tj. koliko ima studenata
+zapisanih u datoteci. Nakon toga potrebno je dinamički alocirati prostor za niz struktura
+studenata (ime, prezime, bodovi) i učitati iz datoteke sve zapise. Na ekran ispisati ime,
+prezime, apsolutni i relativni broj bodova.
+Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na kolokviju.
+relatvan_br_bodova = br_bodova/max_br_bodova*100
+*/
+
+typedef struct {
+	char ime[20];
+	char prezime[20];
+	int bodovi;
+}studenti;
 
 int main()
 {
-	
-	printf("Hello world!");
+	studenti *student;
+	int brojStudenata = 0;
+
+	student = (studenti *)malloc(sizeof(studenti)*10);
+
+	FILE *fp;
+	fp = fopen("TextFile1.txt", "r");
+	while (!feof(fp)) {
+		if (fgetc(fp) == '\n') {
+			brojStudenata++;
+		}
+	}
+	printf("broj studenata je %d", brojStudenata);
+	fclose(fp);
+	free(student);
 	return 0;
 }
